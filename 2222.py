15fd1c5ca8900d1895ed7b296ef5cdbb2aaae75e
Roman S. Nesterov <galeopsis@mail.ru>
2023-11-09 20:45:31 +0700

    v1.11

--- 2222.py
+++ 2222.py
@@ -8,7 +8,7 @@ pygame.init()
 # Устанавливаем размер окна
 window_size = (800, 600)
 window = pygame.display.set_mode(window_size)
-pygame.display.set_caption('Cookie Clicker')
+pygame.display.set_caption('Попади в печеньку')
 speed = 6
 speed_x = speed
 speed_y = speed
@@ -23,7 +23,7 @@ cookie_speed_x, cookie_speed_y = speed_x, speed_y
 
 # Устанавливаем начальный счет
 score = 0
-lastScore = 0
+lastScore = 10
 
 # Основной игровой цикл
 running = True
@@ -37,8 +37,12 @@ while running:
         # Проверяем клик мышью на картинку cookie
         if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
             mouse_x, mouse_y = pygame.mouse.get_pos()
-            if cookie_x <= mouse_x <= cookie_x + cookie_image.get_width() and cookie_y <= mouse_y <= cookie_y + cookie_image.get_height():
+            if (cookie_x <= mouse_x <= cookie_x + cookie_image.get_width()
+                    and cookie_y <= mouse_y <= cookie_y + cookie_image.get_height()):
                 score += 1
+                if score == lastScore:
+                    lastScore += 10
+                    speed += 1
                 print(f'Score: {score}')
                 print(f'Speed: {speed}')
 
@@ -61,11 +65,11 @@ while running:
         cookie_speed_y *= -1
 
     # Отрисовываем cookie и счет на экране
-    window.fill((69, 15, 247))  # Заливаем фон белым цветом
+    window.fill((69, 15, 247))  # Заливаем фон синим цветом
     window.blit(cookie_image, (cookie_x, cookie_y))  # Рисуем cookie
     font = pygame.font.Font(None, 36)
-    text = font.render(f'Score: {score}', True, (15, 247, 100))
-    textSpeed = font.render(f'Speed: {speed}', True, (247, 54, 15))
+    text = font.render(f'Счёт: {score}', True, (15, 247, 100))  # зелёный
+    textSpeed = font.render(f'Скорость: {speed}', True, (247, 54, 15))  # красный
     window.blit(text, (20, 20))  # Рисуем счет
     window.blit(textSpeed, (20, 50))  # Рисуем скорость
 
